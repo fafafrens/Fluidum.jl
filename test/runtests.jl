5f6b77b65e3f772ea7727dfc942a6fba67629ef2
Eduardo <grossi.eduard@gmail.com>
2024-04-29 15:52:49 +0200

    added test simple function

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "Fluidum.jl" begin
     # Write your tests here.
+    @test Fluidum.greet_your_package_name() == "Hello Fluidum!"
+    @test Fluidum.greet_your_package_name() != "Hello world!"
 end
