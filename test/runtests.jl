64e3a1ef9d175eb386aff3994e1bbe4f48cb29e2
Eduardo <grossi.eduard@gmail.com>
2024-04-30 12:03:12 +0200

    added not broking test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,11 @@ using Test
 
 @testset "Fluidum.jl" begin
     # Write your tests here.
-    @test IdealQCD()==IdealQCD{Float64}(3,2)
-    @test isfinite(thermodynamic(1,IdealQCD()))
-    @test isfinite(thermodynamic(0,IdealQCD()))
-    @test isfinite(thermodynamic(10,IdealQCD(1,0)))
+    @test IdealQCD()==IdealQCD(3,2)
+    @test isfinite(first(thermodynamic(1,IdealQCD())))
+    @test isfinite(thermodynamic(0,IdealQCD())[2])
+    @test length(thermodynamic(10,IdealQCD(1,0)))==3
+    @test length(first(thermodynamic(10,IdealQCD(1,0))))==1
+    @test length(last(thermodynamic(10,IdealQCD(1,0))))==1
 
 end
