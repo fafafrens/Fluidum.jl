ca5070cd54f8f7fc1974f0ef1c94ff7c7deec4df
Eduardo <grossi.eduard@gmail.com>
2024-04-30 12:06:51 +0200

    ok now should not be broken

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,11 @@ using Test
 @testset "Fluidum.jl" begin
     # Write your tests here.
     @test IdealQCD()==IdealQCD(3,2)
-    @test isfinite(first(thermodynamic(1,IdealQCD())))
-    @test isfinite(thermodynamic(0,IdealQCD())[2])
+    @test isfinite(thermodynamic(1,IdealQCD()).pressure[1])
+    @test isfinite(thermodynamic(0,IdealQCD()).pressure_derivative[1])
     @test length(thermodynamic(10,IdealQCD(1,0)))==3
-    @test length(first(thermodynamic(10,IdealQCD(1,0))))==1
-    @test length(last(thermodynamic(10,IdealQCD(1,0))))==1
+    @test length(thermodynamic(10,IdealQCD(1,0).pressure))==1
+    @test length(thermodynamic(10,IdealQCD(1,0).pressure_derivative))==1
+    @test length(thermodynamic(10,IdealQCD(1,0).pressure_hessian))==1
 
 end
