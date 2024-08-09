c3fd64272b024b5f08f3aa02f336a613644b71aa
Federica Capellino <capellino@thphys.uni-heidelberg.de>
2024-08-09 11:24:03 +0200

    digits for test

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,7 @@ using ForwardDiff
     @test length(thermodynamic(10,IdealQCD(1,0)).pressure_hessian)==1
 
     @test isfinite(free_charm(1,0,Heavy_Quark())[1])
-    @test pressure(1,Heavy_Quark())==pressure(1,FluiduMEoS())
+    @test round(pressure(1,Heavy_Quark()), sigdigits=10)==round(pressure(1,FluiduMEoS()), sigdigits=10)
     @test round(pressure(1,Heavy_Quark()), sigdigits=10)==round(thermodynamic(1,FluiduMEoS()).pressure[1], sigdigits=10)
 
     @test det(Fluidum.one_d_viscous_matrix([0.2,0.1,0,0,0,-0.1,0],2.,2.,0.5,0.5,0.5,0.5,0.5,0,0,0,1.,1.,0.1,0.1,0.1,1.,0)[1])!=0
